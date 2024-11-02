d8aa7d3b0c018f428c93c35a5c97ca465e6b5480
Thomas Lindop <thomas16256@gmail.com>
2024-11-02 11:57:36 +0000

    >> rewrite prompt to have neater code

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,25 +2,18 @@ function fish_prompt
     set -l last_status $status
     set -l status_prompt
     if test $last_status -ne 0
-        set status_prompt "[31m=>> $last_status <<=[0m\n"
+        set status_prompt "\e[31m=>> $last_status <<=\e[0m\n"
     end
-    echo -e "]133;A\\]133;P\007\
-$status_prompt\
-[33m$USER\
-[0m@\
-[35m$HOSTNAME\
-[0m+\
-[36mfish\
-[0m [\
-[1;34m$(prompt_pwd --dir-length 0)\
-[0m] <\
-[34mjobs??\
-[0m> \
-[3;32m $(git branch --show-current 2>/dev/null)\
-[0m
-[1;31m ~> \
-[0m]133;B\007\
-"
+    printf "$status_prompt%b%b@%b+%b [%b] <%b> %b\n %b %b" \
+        '\e]133;P\007\e]133;A\e\\' \
+        "\e[33m$USER\e[0m" \
+        "\e[35m$HOSTNAME\e[0m" \
+        "\e[36mfish\e[0m" \
+        "\e[1;34m$(prompt_pwd --dir-length 0)\e[0m" \
+        "\e[34mjobs??\e[0m" \
+        "\e[3;32m $(git branch --show-current 2>/dev/null)\e[0m" \
+        "\e[31m~>\e[0m" \
+        '\e]133;B\007'
 end
 
 # function fish_right_prompt
