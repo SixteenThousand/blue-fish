9918165a8065c2beba4cba9d4d2a0998d6b14e8a
Thomas Lindop <thomas16256@gmail.com>
2024-11-02 11:10:25 +0000

    >> rewrite prompt to be more like my bash one > add status of last command

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,17 +1,26 @@
 function fish_prompt
-    # the weird stuff athe start id there so tmux can find prompts
-    echo -e "\033]133;A\033\\\
-$(set_color red)$USER\
-$(set_color normal)@\
-$(set_color blue)$HOSTNAME\
-$(set_color normal)+\
-$(set_color cyan)fish\
-$(set_color normal) in\
-$(set_color bryellow) $(prompt_pwd --dir-length 0)\
-$(set_color brgreen) $(get_git_branch)\
-\n\
-$(set_color red) ~> \
-$(set_color normal)"
+    set -l last_status $status
+    set -l status_prompt
+    if test $last_status -ne 0
+        set status_prompt "[31m=>> $last_status <<=[0m\n"
+    end
+    echo -e "]133;A\\]133;P\007\
+$status_prompt\
+[33m$USER\
+[0m@\
+[35m$HOSTNAME\
+[0m+\
+[36mfish\
+[0m [\
+[1;34m$(prompt_pwd --dir-length 0)\
+[0m] <\
+[34mjobs??\
+[0m> \
+[3;32m $(git branch --show-current 2>/dev/null)\
+[0m
+[1;31m ~> \
+[0m]133;B\007\
+"
 end
 
 # function fish_right_prompt
